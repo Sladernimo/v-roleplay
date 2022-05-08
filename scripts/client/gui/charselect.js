74333c96435f005cd1ce016e3cf53be663ece2fd
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2022-05-08 06:44:08 -0500

    Add titlebar to character select

--- scripts/client/gui/charselect.js
+++ scripts/client/gui/charselect.js
@@ -24,23 +24,26 @@ let characterSelect = {
 
 function initCharacterSelectGUI() {
     logToConsole(LOG_DEBUG, `[VRR.GUI] Creating character select GUI ...`);
-	characterSelect.window = mexui.window(game.width/2-215, game.height/2-83, 430, 190, 'Select Character', {
+	characterSelect.window = mexui.window(game.width/2-215, game.height/2-83, 430, 190, 'SELECT CHARACTER', {
 		main: {
 			backgroundColour: toColour(secondaryColour[0], secondaryColour[1], secondaryColour[2], windowAlpha),
 		},
         title: {
-            textSize: 12.0,
-            textColour: toColour(0, 0, 0, 0),
+			textSize: 12.0,
+			textFont: mainFont,
+            textColour: toColour(0, 0, 0, 255),
             backgroundColour: toColour(primaryColour[0], primaryColour[1], primaryColour[2], windowTitleAlpha),
         },
         icon: {
-            textSize: 12.0,
-            textColour: toColour(0, 0, 0, 0),
+			textSize: 10.0,
+			textFont: mainFont,
+            textColour: toColour(0, 0, 0, 255),
             backgroundColour: toColour(primaryColour[0], primaryColour[1], primaryColour[2], windowTitleAlpha),
         }
 	});
-	characterSelect.window.titleBarIconSize = toVector2(0,0);
-	characterSelect.window.titleBarHeight = 0;
+	characterSelect.window.titleBarIconSize = toVector2(0, 0);
+	characterSelect.window.titleBarIconShown = false;
+	characterSelect.window.titleBarHeight = 30;
 
 	characterSelect.nameText = characterSelect.window.text(5, 40, 200, 25, 'Lastname, Firstname', {
 		main: {
