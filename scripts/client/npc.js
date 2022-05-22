461de67c9a3de52d05bf24136df6132f528ec2a4
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2022-05-22 13:30:19 -0500

    Process NPC movement

---
+++ scripts/client/npc.js
@@ -0,0 +1,19 @@
+// ===========================================================================
+// Vortrex's Roleplay Resource
+// https://github.com/VortrexFTW/gtac_roleplay
+// ===========================================================================
+// FILE: npc.js
+// DESC: Provides NPC functions and processing
+// TYPE: Client (JavaScript)
+// ===========================================================================
+
+function processNPCMovement(npc) {
+	//if(npc.isSyncer == true) {
+		if(getEntityData(npc, "vrr.lookAtClosestPlayer") == true) {
+			let closestPlayer = getClosestPlayer(getElementPosition(npc.id));
+			setPedLookAt(npc, getElementPosition(closestPlayer.id));
+		}
+	//}
+}
+
+// ===========================================================================
