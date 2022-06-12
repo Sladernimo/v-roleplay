519928bf6b41a2e1fbb46114e3579b17dfc3e1fb
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2022-06-12 09:20:08 -0500

    Use locales for random tips (unfinished)

--- scripts/server/timers.js
+++ scripts/server/timers.js
@@ -12,7 +12,7 @@ let serverTimers = {};
 // ===========================================================================
 
 function saveServerDataToDatabase() {
-	if(getServerConfig().pauseSavingToDatabase) {
+	if (getServerConfig().pauseSavingToDatabase) {
 		return false;
 	}
 
@@ -20,67 +20,67 @@ function saveServerDataToDatabase() {
 
 	try {
 		saveAllPlayersToDatabase();
-	} catch(error) {
+	} catch (error) {
 		logToConsole(LOG_ERROR, `Could not save players to database: ${error}`);
 	}
 
 	try {
 		saveAllClansToDatabase();
-	} catch(error) {
+	} catch (error) {
 		logToConsole(LOG_ERROR, `Could not save clans to database: ${error}`);
 	}
 
 	try {
 		saveAllHousesToDatabase();
-	} catch(error) {
+	} catch (error) {
 		logToConsole(LOG_ERROR, `Could not save houses to database: ${error}`);
 	}
 
 	try {
 		saveAllBusinessesToDatabase();
-	} catch(error) {
+	} catch (error) {
 		logToConsole(LOG_ERROR, `Could not save businesses to database: ${error}`);
 	}
 
 	try {
 		saveAllVehiclesToDatabase();
-	} catch(error) {
+	} catch (error) {
 		logToConsole(LOG_ERROR, `Could not save vehicles to database: ${error}`);
 	}
 
 	try {
 		saveAllItemTypesToDatabase();
-	} catch(error) {
+	} catch (error) {
 		logToConsole(LOG_ERROR, `Could not save item types to database: ${error}`);
 	}
 
 	try {
 		saveAllItemsToDatabase();
-	} catch(error) {
+	} catch (error) {
 		logToConsole(LOG_ERROR, `Could not save items to database: ${error}`);
 	}
 
 	try {
 		saveAllJobsToDatabase();
-	} catch(error) {
+	} catch (error) {
 		logToConsole(LOG_ERROR, `Could not save jobs to database: ${error}`);
 	}
 
 	try {
 		saveAllNPCsToDatabase();
-	} catch(error) {
+	} catch (error) {
 		logToConsole(LOG_ERROR, `Could not save NPCs to database: ${error}`);
 	}
 
 	try {
 		saveAllGatesToDatabase();
-	} catch(error) {
+	} catch (error) {
 		logToConsole(LOG_ERROR, `Could not save gates to database: ${error}`);
 	}
 
 	try {
 		saveServerConfigToDatabase();
-	} catch(error) {
+	} catch (error) {
 		logToConsole(LOG_ERROR, `Could not save server config to database: ${error}`);
 	}
 
@@ -91,10 +91,10 @@ function saveServerDataToDatabase() {
 
 function initTimers() {
 	//if(!isDevelopmentServer()) {
-		serverTimers.updatePingsTimer = setInterval(updatePings, 5000);
-		serverTimers.oneMinuteTimer = setInterval(oneMinuteTimerFunction, 60000);
-		serverTimers.fifteenMinuteTimer = setInterval(tenMinuteTimerFunction, 600000);
-		serverTimers.thirtyMinuteTimer = setInterval(thirtyMinuteTimerFunction, 1800000);
+	serverTimers.updatePingsTimer = setInterval(updatePings, 5000);
+	serverTimers.oneMinuteTimer = setInterval(oneMinuteTimerFunction, 60000);
+	serverTimers.fifteenMinuteTimer = setInterval(tenMinuteTimerFunction, 600000);
+	serverTimers.thirtyMinuteTimer = setInterval(thirtyMinuteTimerFunction, 1800000);
 	//}
 }
 
@@ -132,12 +132,12 @@ function thirtyMinuteTimerFunction() {
 
 function checkVehicleRenting() {
 	let renting = getServerData().rentingVehicleCache;
-	for(let i in renting) {
-		if(isClientInitialized(renting[i])) {
-			if(getPlayerData(renting[i]) != false) {
-				if(isPlayerLoggedIn(renting[i] && isPlayerSpawned(renting[i]))) {
-					if(getPlayerData(renting[i]).rentingVehicle != false) {
-						if(getPlayerCurrentSubAccount(renting[i]).cash < getServerData().vehicles[getPlayerData(renting[i]).rentingVehicle].rentPrice) {
+	for (let i in renting) {
+		if (isClientInitialized(renting[i])) {
+			if (getPlayerData(renting[i]) != false) {
+				if (isPlayerLoggedIn(renting[i] && isPlayerSpawned(renting[i]))) {
+					if (getPlayerData(renting[i]).rentingVehicle != false) {
+						if (getPlayerCurrentSubAccount(renting[i]).cash < getServerData().vehicles[getPlayerData(renting[i]).rentingVehicle].rentPrice) {
 							messagePlayerAlert(renting[i], `You do not have enough money to continue renting this vehicle!`);
 							stopRentingVehicle(renting[i]);
 						} else {
@@ -174,11 +174,11 @@ function checkVehicleRenting() {
 
 function updatePings() {
 	let clients = getClients();
-	for(let i in clients) {
-		if(isClientInitialized(clients[i])) {
-			if(!clients[i].console) {
+	for (let i in clients) {
+		if (isClientInitialized(clients[i])) {
+			if (!clients[i].console) {
 				updatePlayerPing(clients[i]);
-				if(isPlayerSpawned(clients[i])) {
+				if (isPlayerSpawned(clients[i])) {
 					updatePlayerCash(clients[i]);
 				}
 			}
@@ -190,16 +190,16 @@ function updatePings() {
 
 function checkServerGameTime() {
 	//if(!getServerConfig().useRealTime) {
-		if(getServerConfig().minute >= 59) {
-			getServerConfig().minute = 0;
-			if(getServerConfig().hour >= 23) {
-				getServerConfig().hour = 0;
-			} else {
-				getServerConfig().hour = getServerConfig().hour + 1;
-			}
+	if (getServerConfig().minute >= 59) {
+		getServerConfig().minute = 0;
+		if (getServerConfig().hour >= 23) {
+			getServerConfig().hour = 0;
 		} else {
-			getServerConfig().minute = getServerConfig().minute + 1;
+			getServerConfig().hour = getServerConfig().hour + 1;
 		}
+	} else {
+		getServerConfig().minute = getServerConfig().minute + 1;
+	}
 	//} else {
 	//	let dateTime = getCurrentTimeStampWithTimeZone(getServerConfig().realTimeZone);
 	//	getServerConfig().hour = dateTime.getHours();
@@ -213,9 +213,9 @@ function checkServerGameTime() {
 
 function checkPayDays() {
 	let clients = getClients();
-	for(let i in clients) {
-		if(isClientInitialized(clients[i])) {
-			if(isPlayerLoggedIn(clients[i]) && isPlayerSpawned(clients[i])) {
+	for (let i in clients) {
+		if (isClientInitialized(clients[i])) {
+			if (isPlayerLoggedIn(clients[i]) && isPlayerSpawned(clients[i])) {
 				getPlayerData(clients[i]).payDayStart = sdl.ticks;
 				playerPayDay(clients[i]);
 
@@ -227,8 +227,8 @@ function checkPayDays() {
 		}
 	}
 
-	for(let i in getServerData().businesses) {
-		if(getBusinessData(i).ownerType != VRR_BIZOWNER_NONE && getBusinessData(i).ownerType != VRR_BIZOWNER_PUBLIC && getBusinessData(i).ownerType != VRR_BIZOWNER_FACTION) {
+	for (let i in getServerData().businesses) {
+		if (getBusinessData(i).ownerType != VRR_BIZOWNER_NONE && getBusinessData(i).ownerType != VRR_BIZOWNER_PUBLIC && getBusinessData(i).ownerType != VRR_BIZOWNER_FACTION) {
 			getBusinessData(i).till += 1000;
 		}
 	}
@@ -237,14 +237,14 @@ function checkPayDays() {
 // ===========================================================================
 
 function showRandomTipToAllPlayers() {
-	let tipId = getRandom(0, randomTips.length-1);
-
 	let clients = getClients();
-	for(let i in clients) {
-		if(isClientInitialized(clients[i])) {
-			if(isPlayerLoggedIn(clients[i]) && isPlayerSpawned(clients[i])) {
-				if(!doesPlayerHaveRandomTipsDisabled(clients[i])) {
-					messagePlayerTimedRandomTip(null, randomTips[tipId]);
+	for (let i in clients) {
+		if (isClientInitialized(clients[i])) {
+			if (isPlayerLoggedIn(clients[i]) && isPlayerSpawned(clients[i])) {
+				if (!doesPlayerHaveRandomTipsDisabled(clients[i])) {
+					let localeId = getPlayerLocaleId(clients[i]);
+					let tipId = getRandom(0, getServerData().localeStrings[localeId]["RandomTips"].length - 1);
+					messagePlayerTip(clients[i], getGroupedLocaleString(clients[i], "RandomTips", tipId));
 				}
 			}
 		}
@@ -255,11 +255,11 @@ function showRandomTipToAllPlayers() {
 
 function checkInactiveVehicleRespawns() {
 	let vehicles = getElementsByType(ELEMENT_VEHICLE);
-	for(let i in vehicles) {
-		if(getVehicleData(vehicles[i] != false)) {
-			if(isVehicleUnoccupied(vehicles[i])) {
-				if(getVehicleData(vehicles[i]).lastActiveTime != false) {
-					if(getCurrentUnixTimestamp() - getVehicleData(vehicles[i]).lastActiveTime >= getGlobalConfig().vehicleInactiveRespawnDelay) {
+	for (let i in vehicles) {
+		if (getVehicleData(vehicles[i] != false)) {
+			if (isVehicleUnoccupied(vehicles[i])) {
+				if (getVehicleData(vehicles[i]).lastActiveTime != false) {
+					if (getCurrentUnixTimestamp() - getVehicleData(vehicles[i]).lastActiveTime >= getGlobalConfig().vehicleInactiveRespawnDelay) {
 						respawnVehicle(vehicles[i]);
 						getVehicleData(vehicles[i]).lastActiveTime = false;
 					}
