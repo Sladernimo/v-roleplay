b86d2c7bcbaaae473996e4a574c4bae12e22ec21
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2022-06-27 16:40:54 -0500

    Use built-in veh enter/exit events

--- scripts/server/event.js
+++ scripts/server/event.js
@@ -32,10 +32,10 @@ function addAllEventHandlers() {
 	addEventHandler("onElementStreamOut", onElementStreamOut);
 
 	addEventHandler("onPedSpawn", onPedSpawn);
-	addEventHandler("onPedEnteringVehicleEx", onPedEnteringVehicle);
-	addEventHandler("onPedExitingVehicleEx", onPedExitingVehicle);
-	addEventHandler("onPedEnteredVehicleEx", onPedEnteredVehicle);
-	addEventHandler("onPedExitedVehicleEx", onPedExitedVehicle);
+	addEventHandler("onPedEnteringVehicle", onPedEnteringVehicle);
+	addEventHandler("onPedExitingVehicle", onPedExitingVehicle);
+	addEventHandler("onPedEnteredVehicle", onPedEnteredVehicle);
+	addEventHandler("onPedExitedVehicle", onPedExitedVehicle);
 
 	//addEventHandler("OnPlayerCommand", onPlayerCommand);
 }
