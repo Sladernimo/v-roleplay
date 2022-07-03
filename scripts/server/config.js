0ec8608b2b1c0d8c73acd4067533526ec66a6e74
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2022-07-02 23:31:40 -0500

    Add ATM distance to global config

--- scripts/server/config.js
+++ scripts/server/config.js
@@ -234,6 +234,7 @@ let globalConfig = {
 	maxChatLines: 6,
 	vehicleTrunkDistance: 2.0,
 	fishingSpotDistance: 10.0,
+	atmDistance: 1.5,
 };
 
 // ===========================================================================
