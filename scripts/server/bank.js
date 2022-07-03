e17f6f3c4e16693eca817c1362d14fe8ff9ed2e6
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2022-07-02 23:30:51 -0500

    Add bank account owner consts

--- scripts/server/bank.js
+++ scripts/server/bank.js
@@ -7,6 +7,17 @@
 // TYPE: Server (JavaScript)
 // ===========================================================================
 
+// House Owner Types
+const VRR_BANK_ACCT_OWNER_NONE = 0;					// Not owned
+const VRR_BANK_ACCT_OWNER_PLAYER = 1;				// Owner is a player (character/subaccount)
+const VRR_BANK_ACCT_OWNER_JOB = 2;					// Owned by a job
+const VRR_BANK_ACCT_OWNER_CLAN = 3;					// Owned by a clan
+const VRR_BANK_ACCT_OWNER_FACTION = 4;				// Owned by a faction
+const VRR_BANK_ACCT_OWNER_BIZ = 4;					// Owned by a faction
+const VRR_BANK_ACCT_OWNER_PUBLIC = 5;				// Is a public bank account. Technically not owned. This probably won't be used.
+
+// ===========================================================================
+
 function isPlayerAtBank(client) {
 	if (isPositionAtATM(getPlayerPosition(client))) {
 		return true;
@@ -14,7 +25,7 @@ function isPlayerAtBank(client) {
 
 	let businessId = getPlayerBusiness(client);
 	if (getBusinessData(client) != false) {
-		if (getBusinessData(businessId).entranceType == AGRP_BIZ_ENTRANCE_TYPE_BANK) {
+		if (getBusinessData(businessId).type == AGRP_BIZ_TYPE_BANK) {
 			return true;
 		}
 	}
@@ -64,3 +75,5 @@ function isPositionAtATM(position) {
 
 	return false;
 }
+
+// ===========================================================================
