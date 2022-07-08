2a0f2443d458f6d7d3ea343a89a503ded88b6610
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2022-07-08 14:04:24 -0500

    Add new GUI script files

---
+++ scripts/client/gui/games/5-card-poker.js
@@ -0,0 +1,9 @@
+// ===========================================================================
+// Asshat Gaming Roleplay
+// https://github.com/VortrexFTW/agrp_main
+// (c) 2022 Asshat Gaming
+// ===========================================================================
+// FILE: 5cardpoker.js
+// DESC: Provides 5-card poker games GUI
+// TYPE: Client (JavaScript)
+// ===========================================================================
---
+++ scripts/client/gui/games/blackjack.js
@@ -0,0 +1,9 @@
+// ===========================================================================
+// Asshat Gaming Roleplay
+// https://github.com/VortrexFTW/agrp_main
+// (c) 2022 Asshat Gaming
+// ===========================================================================
+// FILE: blackjack.js
+// DESC: Provides blackjack game GUI
+// TYPE: Client (JavaScript)
+// ===========================================================================
