56b1825508fbea84cf2f7d0a27705e70a8d98ee8
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2022-09-04 15:49:52 -0500

    Init network events script

--- scripts/client/startup.js
+++ scripts/client/startup.js
@@ -13,7 +13,7 @@ function initClientScripts() {
 	initNameTagScript();
 	initScoreBoardScript();
 	initMessagingScript();
-	initServerScript();
+	initNetworkEventsScript();
 	initLogoScript();
 	initLabelScript();
 	initChatBoxScript();
@@ -21,6 +21,8 @@ function initClientScripts() {
 	initKeyBindScript();
 	initEventScript();
 	initSkinSelectScript();
+
+	addAllNetworkHandlers();
 }
 
 // ===========================================================================
