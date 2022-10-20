6e7a5780cc5f04cde92374ffa60c2e5c6ab850ec
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2022-10-19 23:57:47 -0500

    Add element stream distance getters

--- scripts/server/native/connected.js
+++ scripts/server/native/connected.js
@@ -1212,6 +1212,30 @@ function setElementStreamOutDistance(element, distance) {
 
 // ===========================================================================
 
+function getElementStreamInDistance(element) {
+	if (!isNull(element) && element != false) {
+		if (typeof element == "Entity") {
+			if (typeof element.streamInDistance != "undefined") {
+				return element.streamInDistance;
+			}
+		}
+	}
+}
+
+// ===========================================================================
+
+function getElementStreamOutDistance(element) {
+	if (!isNull(element) && element != false) {
+		if (typeof element == "Entity") {
+			if (typeof element.streamOutDistance != "undefined") {
+				return element.streamOutDistance;
+			}
+		}
+	}
+}
+
+// ===========================================================================
+
 function getPlayerPed(client) {
 	if (isNull(client)) {
 		return null;
