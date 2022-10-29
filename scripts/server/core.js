c75ef6b89ab264d34ac4505b6f456366daf6756f
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2022-10-29 10:07:06 -0500

    Reduce default log level

--- scripts/server/core.js
+++ scripts/server/core.js
@@ -10,7 +10,7 @@
 
 let scriptVersion = "1.2";
 let serverStartTime = 0;
-let logLevel = LOG_INFO | LOG_DEBUG | LOG_VERBOSE; // LOG_ERROR|LOG_WARN;
+let logLevel = LOG_INFO; // LOG_ERROR|LOG_WARN;
 
 let playerResourceReady = new Array(server.maxClients).fill(false);
 let playerResourceStarted = new Array(server.maxClients).fill(false);
