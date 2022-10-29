1eac24669f12b44f33435c2d648e442126c9d891
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2022-10-29 10:05:28 -0500

    Force ground snow

--- scripts/client/utilities.js
+++ scripts/client/utilities.js
@@ -224,13 +224,11 @@ function setLocalPlayerInterior(interior) {
 
 // ===========================================================================
 
-function setSnowState(falling, ground) {
+function setSnowState(falling, ground, forceGround) {
 	logToConsole(LOG_DEBUG, `[AGRP.Utilities] Setting falling snow to ${falling} and ground snow to ${ground}`);
 	snowing = falling;
-	if (ground) {
-		forceSnowing(false);
-		forceSnowing(ground);
-	}
+	snow.enabled = ground;
+	forceSnowing(forceGround);
 }
 
 // ===========================================================================
