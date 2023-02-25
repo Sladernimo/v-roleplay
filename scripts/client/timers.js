69317b3b1f7f8f52887cd501761b396d266d118a
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2023-02-25 04:39:27 -0600

    Add client timers script

---
+++ scripts/client/timers.js
@@ -0,0 +1,20 @@
+// ===========================================================================
+// Vortrex's Roleplay Resource
+// https://github.com/VortrexFTW/v-roleplay
+// ===========================================================================
+// FILE: timers.js
+// DESC: Provides timer functions and usage
+// TYPE: Client (JavaScript)
+// ===========================================================================
+
+function initTimersScript() {
+	logToConsole(LOG_DEBUG, "[V.RP.Timers]: Initializing timer script ...");
+	logToConsole(LOG_DEBUG, "[V.RP.Timers]: Timers script initialized!");
+}
+
+// ===========================================================================
+
+function initTimers() {
+}
+
+// ===========================================================================
