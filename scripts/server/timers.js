dbef2a2d5d1789a62269db0e4783b97f8debc736
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2023-03-05 20:44:13 -0600

    Fix desynced payphones in timer

--- scripts/server/timers.js
+++ scripts/server/timers.js
@@ -122,6 +122,7 @@ function oneMinuteTimerFunction() {
 	}
 
 	updateVehicleSavedPositions();
+	fixDesyncedPayPhones();
 }
 
 // ===========================================================================
