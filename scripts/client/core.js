cac3c9484abac61bf99fcf13faf563d35f8b0a13
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2023-03-10 11:38:04 -0600

    Disable debug logging on client

--- scripts/client/core.js
+++ scripts/client/core.js
@@ -28,7 +28,7 @@ let renderHotBar = true;
 let renderItemActionDelay = true;
 let renderInteriorLights = true;
 
-let logLevel = LOG_INFO | LOG_DEBUG;
+let logLevel = LOG_INFO;
 
 let weaponDamageEnabled = {};
 let weaponDamageEvent = {};
