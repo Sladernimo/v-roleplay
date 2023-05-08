b409321c0deb1238fe28b6ccc65e041cff176035
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2023-05-07 23:20:42 -0500

    Formatting

--- scripts/client/sync.js
+++ scripts/client/sync.js
@@ -14,7 +14,7 @@ function processSync(event, deltaTime) {
 			sendNetworkEventToServer("v.rp.plr.rot", (localPlayer.vehicle != null) ? localPlayer.vehicle.heading : localPlayer.heading);
 
 			//if(localPlayer.vehicle != null) {
-			//    sendNetworkEventToServer("v.rp.veh.pos", getVehicleForNetworkEvent(localPlayer.vehicle), localPlayer.vehicle.position);
+			//	sendNetworkEventToServer("v.rp.veh.pos", getVehicleForNetworkEvent(localPlayer.vehicle), localPlayer.vehicle.position);
 			//    sendNetworkEventToServer("v.rp.veh.rot", getVehicleForNetworkEvent(localPlayer.vehicle), localPlayer.vehicle.heading);
 			//}
 		}
